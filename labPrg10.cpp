546c0c6cfd1d73c29c8298f9ce96b49c4bcff550
Ram Priyatham <75263348+Ram-Priyatham@users.noreply.github.com>
2020-12-03 21:10:23 +0530

    Update labPrg10.cpp

--- labPrg10.cpp
+++ labPrg10.cpp
@@ -5,35 +5,187 @@ copy constructor and assignment operators>, <, >=, <=, ==, ++ (pre and post), +,
 using namespace std;
 class vectors
 {
-	int n,*a;
+	int n;
+	vector<int> v;
 	public:
 		vectors()
 		{
 		}
-		vectors(int n)
+		vectors(int x)
 		{
-			a=new int[n];
-			cout<<"Enter array elements"<<endl;
+			n=x;
+		}
+		vectors operator <(const vectors &rhs)
+		{
+			int c1=0,c2=0;
 			for(int i=0;i<n;i++)
 			{
-				cin>>a[i];
+				if(v[i]<rhs.v[i])
+				{
+					c1++;
+				}
+				else if(v[i]>rhs.v[i])
+				{
+					c2++;
+				}
 			}
-			cout<<endl;
+			if(c1<c2)
+			{
+				cout<<"2nd vector is smaller"<<endl;
+			}
+			else
+			{
+				cout<<"1st vector is smaller"<<endl;
+			}
+		}
+		vectors operator >(const vectors &rhs)
+		{
+			int c1=0,c2=0;
+			for(int i=0;i<n;i++)
+			{
+				if(v[i]>rhs.v[i])
+				{
+					c1++;
+				}
+				else if(v[i]<rhs.v[i])
+				{
+					c2++;
+				}
+			}
+			if(c1>c2)
+			{
+				cout<<"1st vector is larger"<<endl;
+			}
+			else if(c1<c2)
+			{
+				cout<<"2nd vector is larger"<<endl;
+			}
+		}
+		vectors operator <=(const vectors &rhs)
+		{
+			int c1=0,c2=0;
+			for(int i=0;i<n;i++)
+			{
+				if(v[i]<rhs.v[i])
+				{
+					c1++;
+				}
+				else if(v[i]>rhs.v[i])
+				{
+					c2++;
+				}
+			}
+			if(c1<c2)
+			{
+				cout<<"2nd vector is smaller"<<endl;
+			}
+			else if(c1==c2)
+			{
+				cout<<"Both are same "<<endl;
+			}
+			else
+			{
+				cout<<"1st vector is smaller"<<endl;
+			}
+		}
+		vectors operator >=(const vectors &rhs)
+		{
+			int c1=0,c2=0;
+			for(int i=0;i<n;i++)
+			{
+				if(v[i]>rhs.v[i])
+				{
+					c1++;
+				}
+				else if(v[i]<rhs.v[i])
+				{
+					c2++;
+				}
+			}
+			if(c1>c2)
+			{
+				cout<<"1st vector is larger"<<endl;
+			}
+			else if(c1==c2)
+			{
+				cout<<"Both are equal"<<endl;
+			}
+			else
+			{
+				cout<<"2nd vector is larger"<<endl;
+			}
+		}
+		vectors operator ==(const vectors &rhs)
+		{
+			int cnt=0;
+			for(int i=0;i<n;i++)
+			{
+				if(v[i]==rhs.v[i])
+				{
+					cnt++;
+				}
+			}
+			if(cnt==n)
+			{
+				cout<<"Both vectors are equal"<<endl;
+			}
+			else
+				cout<<"Both vectors are not equal"<<endl;
+		}
+		vectors operator +=(const vectors &rhs)
+		{
+			for(int i=0;i<n;i++)
+			{
+				v[i]=v[i]+rhs.v[i];
+			}
+		}
+		vectors operator +(const vectors &rhs)
+		{
+			
+			for(int i=0;i<n;i++)
+			{
+				
+			}
+		}
+		vectors operator =(const vectors &rhs)
+		{
+			v=rhs.v;
+			cout<<"Assignment operator printing "<<endl;
+			return *this;
 		}
 		void print()
 		{
-			cout<<"Vector elements are "<<endl;
+			for(int i=0;i<v.size();i++)
+			{
+				cout<<v[i]<<endl;
+			}
+		}
+		void enter()
+		{
+			int t;
+			cout<<"Enter elements "<<endl;
 			for(int i=0;i<n;i++)
 			{
-				cout<<a[i]<<" ";
+				cin>>t;
+				v.push_back(t);
 			}
-			cout<<endl;
-		}
-		
+		}	
 };
 
 int main()
 {
-	vectors v1(5);
+	vectors v1(3);
+	v1.enter();
+	vectors v2;
+	/*v2=v1;
+	v2.print();*/
+	vectors v3(3);
+	v3.enter();
+	/*v1<v3;
+	v1<=v3;
+	v1>v3;
+	v1>=v3;
+	v1==v3;*/
+	v1+=v3;
 	v1.print();
 }
